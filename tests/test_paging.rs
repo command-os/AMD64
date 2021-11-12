3ee6017aa3fc66223810f5d8f068b5baa06eef9b
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2021-11-12 22:54:42 +0200

    Remove high half paging tests

--- tests/test_paging.rs
+++ tests/test_paging.rs
@@ -3,7 +3,7 @@ use amd64::paging::Pml4;
 #[test]
 fn test_map_page() {
     unsafe {
-        let mut pml4 = amd64::paging::PageTable::new();
+        let mut pml4 = Box::new(amd64::paging::PageTable::new());
         pml4.map_pages(amd64::paging::PHYS_VIRT_OFFSET, 0x20_0000, 3);
 
         for i in 0..3 {
@@ -18,44 +18,13 @@ fn test_map_page() {
 #[test]
 fn test_map_huge_page() {
     unsafe {
-        let mut pml4 = amd64::paging::PageTable::new();
+        let mut pml4 = Box::new(amd64::paging::PageTable::new());
         pml4.map_huge_pages(amd64::paging::PHYS_VIRT_OFFSET, 0x20_0000, 3);
 
         for i in 0..3 {
             assert_eq!(
                 pml4.virt_to_phys(amd64::paging::PHYS_VIRT_OFFSET + 0x20_0000 * i),
                 Some(0x20_0000 + 0x20_0000 * i)
-            );
-        }
-    }
-}
-
-#[test]
-fn test_map_higher_half_phys() {
-    unsafe {
-        let mut pml4 = amd64::paging::PageTable::new();
-        pml4.map_higher_half();
-
-        assert_eq!(pml4.virt_to_phys(amd64::paging::PHYS_VIRT_OFFSET), None);
-        for i in 0..2047 {
-            assert_eq!(
-                pml4.virt_to_phys(amd64::paging::PHYS_VIRT_OFFSET + 0x20_0000 + 0x20_0000 * i),
-                Some(0x20_0000 + 0x20_0000 * i)
-            );
-        }
-    }
-}
-
-#[test]
-fn test_map_higher_half_kern() {
-    unsafe {
-        let mut pml4 = amd64::paging::PageTable::new();
-        pml4.map_higher_half();
-
-        for i in 0..1024 {
-            assert_eq!(
-                pml4.virt_to_phys(amd64::paging::KERNEL_VIRT_OFFSET + 0x20_0000 * i),
-                Some(0x20_0000 * i)
             );
         }
     }
