833dc30db3402a27da25c2d91d9297688175f3dc
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2021-11-16 17:29:22 +0200

    Fix tests

--- tests/test_paging.rs
+++ tests/test_paging.rs
@@ -11,8 +11,7 @@ fn test_map_higher_half_phys() {
         let mut pml4 = Box::new(Pml4::new());
         pml4.map_higher_half();
 
-        assert_eq!(pml4.virt_to_phys(amd64::paging::PHYS_VIRT_OFFSET), None);
-        for i in 1..2048 {
+        for i in 0..2048 {
             assert_eq!(
                 pml4.virt_to_phys(amd64::paging::PHYS_VIRT_OFFSET + 0x20_0000 * i),
                 Some(0x20_0000 * i)
