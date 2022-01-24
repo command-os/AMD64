c0f5228a8df553d4b7619764f7d3d99c6537b5ac
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2022-01-24 13:46:47 +0200

    Make Paging impl a trait, fix flags being ignored

--- src/paging/pml4.rs
+++ src/paging/pml4.rs
@@ -3,178 +3,160 @@
  * This project is licensed by the Creative Commons Attribution-NoCommercial-NoDerivatives licence.
  */
 
-#[macro_export]
-macro_rules! impl_pml4 {
-    ($expression:expr, $virt_off:expr) => {
-        use core::arch::asm;
+use core::arch::asm;
 
-        #[repr(transparent)]
-        #[derive(Debug)]
-        pub struct Pml4(pub amd64::paging::PageTable);
+pub trait Pml4: Sized {
+    const VIRT_OFF: usize;
 
-        impl core::ops::Deref for Pml4 {
-            type Target = amd64::paging::PageTable;
+    fn get_entry(&mut self, offset: usize) -> &mut super::PageTableEntry;
+    fn alloc_entry() -> usize;
 
-            fn deref(&self) -> &Self::Target {
-                &self.0
-            }
+    /// # Safety
+    /// The caller must ensure that this operation has no unsafe side effects.
+    #[inline]
+    unsafe fn set(&mut self) {
+        asm!("mov cr3, {}", in(reg) self as *mut _ as usize - Self::VIRT_OFF, options(nostack, preserves_flags));
+    }
+
+    /// # Safety
+    /// The caller must ensure that this operation has no unsafe side effects.
+    #[inline]
+    unsafe fn get() -> &'static mut Self {
+        let pml4: *mut Self;
+        asm!("mov {}, cr3", out(reg) pml4, options(nostack, preserves_flags));
+        pml4.as_mut().unwrap()
+    }
+
+    /// # Safety
+    /// The caller must ensure that this operation has no unsafe side effects.
+    #[inline]
+    unsafe fn get_or_alloc_entry(
+        &mut self,
+        offset: usize,
+        flags: super::PageTableEntry,
+    ) -> &mut Self {
+        let entry = self.get_entry(offset);
+
+        if !entry.present() {
+            entry.set_address((Self::alloc_entry() >> 12) as u64);
+            entry.set_present(flags.present());
+            entry.set_writable(flags.writable());
+            entry.set_user(flags.user());
+            entry.set_pat0(flags.pat0());
+            entry.set_pat1(flags.pat1());
+            entry.set_huge_or_pat2(flags.huge_or_pat2());
+            entry.set_global(flags.global());
+            entry.set_no_execute(flags.no_execute());
         }
 
-        impl core::ops::DerefMut for Pml4 {
-            fn deref_mut(&mut self) -> &mut Self::Target {
-                &mut self.0
+        (((entry.address() << 12) as usize + Self::VIRT_OFF) as *mut Self)
+            .as_mut()
+            .unwrap()
+    }
+
+    /// # Safety
+    /// The caller must ensure that this operation has no unsafe side effects.
+    #[inline]
+    unsafe fn get_or_null_entry(&mut self, offset: usize) -> Option<&mut Self> {
+        let entry = self.get_entry(offset);
+
+        if !entry.present() {
+            None
+        } else {
+            Some(
+                (((entry.address() << 12) as usize + Self::VIRT_OFF) as *mut Self)
+                    .as_mut()
+                    .unwrap(),
+            )
+        }
+    }
+
+    /// # Safety
+    /// The caller must ensure that this operation has no unsafe side effects.
+    #[inline]
+    unsafe fn virt_to_phys(&mut self, virt: usize) -> Option<usize> {
+        let offs = super::PageTableOffsets::new(virt);
+        let pdp = self.get_or_null_entry(offs.pml4)?;
+        let pd = pdp.get_or_null_entry(offs.pdp)?;
+
+        if pd.get_entry(offs.pd).huge_or_pat2() {
+            Some((pd.get_entry(offs.pd).address() << 12) as usize)
+        } else {
+            let pt = pd.get_or_null_entry(offs.pd)?;
+
+            if pt.get_entry(offs.pt).present() {
+                Some((pt.get_entry(offs.pt).address() << 12) as usize)
+            } else {
+                None
             }
         }
+    }
 
-        impl Pml4 {
-            fn alloc_entry() -> usize {
-                $expression
-            }
-
-            pub fn new() -> Self {
-                Self(amd64::paging::PageTable::new())
-            }
-
-            /// # Safety
-            /// The caller must ensure that this operation has no unsafe side effects.
-            #[inline]
-            pub unsafe fn set(&mut self) {
-                asm!("mov cr3, {}", in(reg) self as *mut _ as usize - $virt_off, options(nostack, preserves_flags));
-            }
-
-            /// # Safety
-            /// The caller must ensure that this operation has no unsafe side effects.
-            #[inline]
-            pub unsafe fn get() -> &'static mut Self {
-                let pml4: *mut Self;
-                asm!("mov {}, cr3", out(reg) pml4, options(nostack, preserves_flags));
-                pml4.as_mut().unwrap()
-            }
-
-            /// # Safety
-            /// The caller must ensure that this operation has no unsafe side effects.
-            #[inline]
-            pub unsafe fn get_or_alloc_entry(
-                &mut self,
-                offset: usize,
-                flags: amd64::paging::PageTableEntry,
-            ) -> &mut Self {
-                let entry = &mut self.entries[offset];
-
-                if !entry.present() {
-                    entry.set_address((Self::alloc_entry() >> 12) as u64);
-                    entry.set_present(flags.present());
-                    entry.set_writable(flags.writable());
-                    entry.set_user(flags.user());
-                    entry.set_pat0(flags.pat0());
-                    entry.set_pat1(flags.pat1());
-                    entry.set_huge_or_pat2(flags.huge_or_pat2());
-                    entry.set_global(flags.global());
-                    entry.set_no_execute(flags.no_execute());
-                }
-
-                (((entry.address() << 12) as usize + $virt_off) as *mut Self).as_mut().unwrap()
-            }
-
-            /// # Safety
-            /// The caller must ensure that this operation has no unsafe side effects.
-            #[inline]
-            pub unsafe fn get_or_null_entry(&self, offset: usize) -> Option<&mut Self> {
-                let entry = &self.entries[offset];
-
-                if !entry.present() {
-                    None
-                } else {
-                    Some((((entry.address() << 12) as usize + $virt_off) as *mut Self).as_mut().unwrap())
-                }
-            }
-
-            /// # Safety
-            /// The caller must ensure that this operation has no unsafe side effects.
-            #[inline]
-            pub unsafe fn virt_to_phys(&self, virt: usize) -> Option<usize> {
-                let offs = amd64::paging::PageTableOffsets::new(virt);
-                let pdp = self.get_or_null_entry(offs.pml4)?;
-                let pd = pdp.get_or_null_entry(offs.pdp)?;
-
-                if pd.entries[offs.pd].huge_or_pat2() {
-                    Some((pd.entries[offs.pd].address() << 12) as usize)
-                } else {
-                    let pt = pd.get_or_null_entry(offs.pd)?;
-
-                    if pt.entries[offs.pt].present() {
-                        Some((pt.entries[offs.pt].address() << 12) as usize)
-                    } else {
-                        None
-                    }
-                }
-            }
-
-            /// # Safety
-            /// The caller must ensure that this operation has no unsafe side effects.
-            #[inline]
-            pub unsafe fn map_pages(&mut self, virt: usize, phys: usize, count: usize, flags: amd64::paging::PageTableEntry) {
-                let flags = amd64::paging::PageTableEntry::new()
-                    .with_present(true)
-                    .with_writable(true)
-                    .with_user(true);
-
-                for i in 0..count {
-                    let physical_address = phys + 0x1000 * i;
-                    let virtual_address = virt + 0x1000 * i;
-                    let offs = amd64::paging::PageTableOffsets::new(virtual_address);
-                    let pdp = self.get_or_alloc_entry(offs.pml4, flags);
-                    let pd = pdp.get_or_alloc_entry(offs.pdp, flags);
-                    let pt = pd.get_or_alloc_entry(offs.pd, flags);
-                    pt.entries[offs.pt] = flags
-                        .with_present(true)
-                        .with_address((physical_address >> 12) as u64);
-                }
-            }
-
-            /// # Safety
-            /// The caller must ensure that this operation has no unsafe side effects.
-            #[inline]
-            pub unsafe fn map_huge_pages(&mut self, virt: usize, phys: usize, count: usize, flags: amd64::paging::PageTableEntry) {
-                let flags = amd64::paging::PageTableEntry::new()
-                    .with_present(true)
-                    .with_writable(true)
-                    .with_user(true);
-
-                for i in 0..count {
-                    let physical_address = phys + 0x20_0000 * i;
-                    let virtual_address = virt + 0x20_0000 * i;
-                    let offs = amd64::paging::PageTableOffsets::new(virtual_address);
-                    let pdp = self.get_or_alloc_entry(offs.pml4, flags);
-                    let pd = pdp.get_or_alloc_entry(offs.pdp, flags);
-                    pd.entries[offs.pd] = flags
-                        .with_present(true)
-                        .with_huge_or_pat2(true)
-                        .with_address((physical_address >> 12) as u64);
-                }
-            }
-
-            /// # Safety
-            /// The caller must ensure that this operation has no unsafe side effects.
-            #[inline]
-            pub unsafe fn map_higher_half(&mut self) {
-                self.map_huge_pages(
-                    amd64::paging::PHYS_VIRT_OFFSET,
-                    0,
-                    2048,
-                    amd64::paging::PageTableEntry::new()
-                        .with_present(true)
-                        .with_writable(true)
-                );
-                self.map_huge_pages(
-                    amd64::paging::KERNEL_VIRT_OFFSET,
-                    0,
-                    1024,
-                    amd64::paging::PageTableEntry::new()
-                        .with_present(true)
-                        .with_writable(true)
-                );
-            }
+    /// # Safety
+    /// The caller must ensure that this operation has no unsafe side effects.
+    #[inline]
+    unsafe fn map_pages(
+        &mut self,
+        virt: usize,
+        phys: usize,
+        count: usize,
+        flags: super::PageTableEntry,
+    ) {
+        for i in 0..count {
+            let physical_address = phys + 0x1000 * i;
+            let virtual_address = virt + 0x1000 * i;
+            let offs = super::PageTableOffsets::new(virtual_address);
+            let pdp = self.get_or_alloc_entry(offs.pml4, flags);
+            let pd = pdp.get_or_alloc_entry(offs.pdp, flags);
+            let pt = pd.get_or_alloc_entry(offs.pd, flags);
+            *pt.get_entry(offs.pt) = flags
+                .with_present(true)
+                .with_address((physical_address >> 12) as u64);
         }
-    };
+    }
+
+    /// # Safety
+    /// The caller must ensure that this operation has no unsafe side effects.
+    #[inline]
+    unsafe fn map_huge_pages(
+        &mut self,
+        virt: usize,
+        phys: usize,
+        count: usize,
+        flags: super::PageTableEntry,
+    ) {
+        for i in 0..count {
+            let physical_address = phys + 0x20_0000 * i;
+            let virtual_address = virt + 0x20_0000 * i;
+            let offs = super::PageTableOffsets::new(virtual_address);
+            let pdp = self.get_or_alloc_entry(offs.pml4, flags);
+            let pd = pdp.get_or_alloc_entry(offs.pdp, flags);
+            *pd.get_entry(offs.pd) = flags
+                .with_present(true)
+                .with_huge_or_pat2(true)
+                .with_address((physical_address >> 12) as u64);
+        }
+    }
+
+    /// # Safety
+    /// The caller must ensure that this operation has no unsafe side effects.
+    #[inline]
+    unsafe fn map_higher_half(&mut self) {
+        self.map_huge_pages(
+            super::PHYS_VIRT_OFFSET,
+            0,
+            2048,
+            super::PageTableEntry::new()
+                .with_present(true)
+                .with_writable(true),
+        );
+        self.map_huge_pages(
+            super::KERNEL_VIRT_OFFSET,
+            0,
+            1024,
+            super::PageTableEntry::new()
+                .with_present(true)
+                .with_writable(true),
+        );
+    }
 }
