8600b712d43a24137dc519e9e0fa5e8a3a574109
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2022-05-01 22:54:41 +0300

    Add IA32_APIC_BASE

---
+++ src/registers/msr/apic.rs
@@ -0,0 +1,23 @@
+//! Copyright (c) VisualDevelopment 2021-2022.
+//! This project is licensed by the Creative Commons Attribution-NoCommercial-NoDerivatives licence.
+
+use modular_bitfield::prelude::*;
+
+#[bitfield(bits = 64)]
+#[derive(BitfieldSpecifier, Debug, Default, Clone, Copy)]
+#[repr(u64)]
+pub struct ApicBase {
+    #[skip]
+    __: u8,
+    #[skip(setters)]
+    pub bsp: bool,
+    #[skip]
+    __: bool,
+    pub x2apic_enabled: bool,
+    pub apic_global_enable: bool,
+    pub apic_base: B52,
+}
+
+impl super::Msr for ApicBase {
+    const MSR_NUM: u32 = 0x1B;
+}
